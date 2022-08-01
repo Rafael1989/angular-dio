972be13ad8bcd69dfb99cfab5d851834c74a7d79
Rafael <flns_rafa@hotmail.com>
2022-08-01 18:03:08 +0200

    angular

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { Error404Component } from './error-404/error-404.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -17,20 +18,24 @@ import { StarComponent } from './star/star.component';
     StarComponent,
     ReplacePipe,
     NavBarComponent,
-    Error404Component
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
+        path: 'courses',
+        component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
         path: '', 
         redirectTo: 'courses',
         pathMatch: 'full'
-      },
-      {
-        path: 'courses',
-        component: CourseListComponent
       },
       {
         path: '**', component: Error404Component
